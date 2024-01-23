67a9a484613ae770bcc0dadcc2b18076957bfa56
Abel Sánchez <abelsan38@gmail.com>
2024-01-23 16:31:23 -0700

    primer commit

---
+++ useTodos.js
@@ -0,0 +1,46 @@
+import { useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+const initialState = [];
+const init = () => {
+    return JSON.parse(localStorage.getItem("todos")) || [];
+};
+export const useTodos = () => {
+    const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+
+
+
+    const handleNewTodo = (todo) => {
+        const action = {
+            type: "[TODO] Add Todo",
+            payload: todo,
+        };
+        dispatch(action);
+    };
+    const handleDelateTodo = (id) => {
+        dispatch({
+            type: "[TODO] Remove Todo",
+            payload: id,
+        });
+    };
+    const handleTogleTodo = (id) => {
+        dispatch({
+            type: "[TODO] Toggle Todo",
+            payload: id,
+        });
+    };
+    const todosCount = () => {
+        return todos.length
+    }
+    const pendingTodosCount = () => {
+        return todos.filter((todo) => !todo.done).length
+    }
+    return {
+        todos,
+        handleNewTodo,
+        handleDelateTodo,
+        handleTogleTodo,
+        todosCount,
+        pendingTodosCount
+    }
+}
